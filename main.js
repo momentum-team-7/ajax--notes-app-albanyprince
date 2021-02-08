0fb378b60dd7d7b49b3f057c29d94abc2e084a21
Albany <albany.prince@gmail.com>
2021-02-08 09:21:22 -0500

    some styling added, but something happened and I'm unable to get my notes to show.

--- main.js
+++ main.js
@@ -1,11 +1,36 @@
 /* globals fetch, moment */
 const url = 'http://localhost:3000/notes/'
 // const form = document.querySelector('')
-const lilButton = document.getElementById('lil-Button')
+// const lilButton = document.getElementById('lil-Button')
 
 const notesContainer = document.getElementById('notes-container')
-        
-// lilButton.addEventListener('click',listAllNotes)                                        
+
+     
+// lilButton.addEventListener('click',listAllNotes)      
+let form = document.querySelector('form');                         
+
+form.addEventListener('submit', event => {
+    const noteTitle = document.getElementsByClassName('note-title').value
+
+    const noteTextBox = document.getElementsByClassName('notes-text-box').value  
+    
+    createNote(noteTitle, noteTextBox)
+})       
+
+// notesContainer.addEventListener('click', (event) => {
+    if (event.target.classList.contains('edit')){
+        editNote(event.target)
+    }
+    if (event.target.classList.contains('delete')) {
+        deleteNote(event.target)
+    }
+    if (event.target.classList.contains ('update-note')){
+        updateNote(event.target)
+    }
+    if (event.target.classList.contains('cancel')){
+        hideEditControls(event.target.parentElement)
+    }
+})
 
 function listAllNotes() {
 fetch(url)
@@ -21,22 +46,27 @@ fetch(url)
         }
     })
 }
-function renderNoteItem(note){
-    const noteEl = document.createElement('li')
-    noteEl.id = note.id   
-    renderNoteText(noteEl, note) 
+
+function renderNoteItem(noteObj){
+    const noteEl = document.createElement('p')
+    noteEl.id = noteObj.id  
+    renderNoteText(noteEl, noteObj) 
     notesContainer.appendChild(noteEl)  
 } 
-function renderNoteText (noteEl, note) {
-noteEl.innerHTML = `<p>${note.body}</p>`
+
+function renderNoteText (noteEl, noteObj) {
+noteEl.innerHTML = `<p>${noteObj.body}</p>`
 }  
 
-function createNote (noteText) {
+function createNote (noteTitle, noteTextBox) {
+    
+    
     fetch(url, {
         method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
             title:noteTitle,
-            body: noteText
+            body: noteTextBox,
         })
     })
         .then(res => res.json())
@@ -44,9 +74,47 @@ function createNote (noteText) {
             console.log(data)
             renderNoteItem(data)
         })
+       
+
+    }
+
+function deleteNote (element){
+    const noteId = element.parentElement.id
+        fetch(`http://localhost:300/notes@{noteId}`, {method: 'DELETE'})
+        .then(function () {
+            element.parentElement.remove()
+        })
 }
+
+
+
+//     function updateNote (element) {
+//         let noteTitle = document.querySelector(".note-title").value
+//         let noteTextBox = document.querySelector(".note-text-box").value
+//         fetch(`http://localhost:3000/todos/${NoteId}`, {
+//             method: 'PATCH',
+//             headers: { 'Content-Type': 'application/json' },
+//             body: JSON.stringify({
+//                 item: noteTextBox.value,
+//                 // updated_at: moment().format()
+//     })
+//   })
+//     .then(function (res) {
+//       return res.json()
+//     })
+//     .then(function (data) {
+//       console.log(data)
+//       // update the item in the DOM
+//       renderNoteText(data)
+//     })
+    
+//     }
+
+
+
 listAllNotes()
 
-function addNewNote() {
-
-}
+// function addNewNote() {
+//     let noteTitle = document.querySelector(".note-title").value
+//     let noteTextBox = document.querySelector(".note-text-box").value
+// }
